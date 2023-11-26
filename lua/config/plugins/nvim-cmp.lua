03f28eb1c09e066d59a42cbfc88fe4a1288da8cf
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 15:58:11 +0000

    Sync nvim config

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -2,10 +2,10 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-buffer', -- recommends text from the current buffer
-    'hrsh7th/cmp-path', -- recommends file system paths
-    'L3MON4D3/LuaSnip', -- snippet engine
-    'saadparwaiz1/cmp', -- autocomplete snippets
+    'hrsh7th/cmp-buffer',          -- recommends text from the current buffer
+    'hrsh7th/cmp-path',            -- recommends file system paths
+    'L3MON4D3/LuaSnip',            -- snippet engine
+    'saadparwaiz1/cmp',            -- autocomplete snippets
     'rafamadriz/friendly-snippets' -- set of predefined snippets
   },
   config = function()
@@ -40,24 +40,24 @@ return {
         completeopt = 'menu,menuone,preview,noselect'
       },
       snippet = {
-        expand = function (args)
+        expand = function(args)
           luasnip.lsp_expand(args.body)
         end
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping. select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping. scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping. scroll_docs (4),
-        ["<C-Space>"] = cmp.mapping. complete(), -- show completion suggestions
-        ["«C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping. confirm({ select = false }),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),  -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(),  -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),  -- show completion suggestions
+        ["«C-e>"] = cmp.mapping.abort(),         -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-      { name = "luasnip" }, -- snippets 
-      { name = "buffer" }, -- text within current buffer
-      { name = "path" }, -- file system paths
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within current buffer
+        { name = "path" },  -- file system paths
       }),
     })
   end
