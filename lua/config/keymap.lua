a06b0ab1724397569df1e077c5de4c58a11f9964
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 14:56:23 +0000

    Sync nvim config

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -108,7 +108,7 @@ end)
 
 
 -- makes file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("w")
