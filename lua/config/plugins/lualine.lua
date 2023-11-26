2a37dc3398f627da8ef4abbb49191e6b32613e3b
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 13:30:20 +0000

    Sync nvim config

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -21,13 +21,25 @@ return {
               return os.date("%a %H:%M")
             end
           },
-          -- {
-          --   function ()
-          --     local status = require('lsp-status').status()
-          --     vim.notify(status == )
-          --     return status
-          --   end
-          -- },
+          {
+            -- function ()
+            --   local status = require('lsp-status').status()
+            --   vim.notify(status == )
+            --   return status
+            -- end
+            function()
+              local clients = vim.lsp.get_active_clients()
+              if next(clients) ~= nil then
+                local lsp_client_names = {}
+                for _, client in ipairs(clients) do
+                  table.insert(lsp_client_names, client.name)
+                end
+                return "LSP: " .. table.concat(lsp_client_names, ", ")
+              else
+                return "No LSP Connected"
+              end
+            end,
+          },
           "filetype",
         },
         lualine_y = {
