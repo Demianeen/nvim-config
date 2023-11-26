75598feb7dc4df0b25bfedca74d3939ba532e93e
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 13:44:57 +0000

    Sync nvim config

--- lua/config/plugins/lsp/lsp-zero.lua
+++ lua/config/plugins/lsp/lsp-zero.lua
@@ -8,10 +8,21 @@ return {
       -- Disable automatic setup, we are doing it manually
       vim.g.lsp_zero_extend_cmp = 0
       vim.g.lsp_zero_extend_lspconfig = 0
+
+      local lsp_zero = require('lsp-zero')
+      local lsp_status = require('lsp-status')
+
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_status.on_attach(client)
+        lsp_zero.default_keymaps({ buffer = bufnr })
+      end)
+
+      local capabilities = vim.tbl_extend('keep', lsp_zero.capabilities() or {}, lsp_status.capabilities)
+      lsp_zero.set_capabilities(capabilities)
     end,
   },
 
-  { 
+  {
     'williamboman/mason.nvim',
     lazy = false,
     config = true,
@@ -22,7 +33,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {'L3MON4D3/LuaSnip'},
+      { 'L3MON4D3/LuaSnip' },
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
@@ -50,11 +61,11 @@ return {
   -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'williamboman/mason-lspconfig.nvim' },
     },
     config = function()
       -- This is where all the LSP shenanigans will live
@@ -64,7 +75,7 @@ return {
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
+        lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
       require('mason-lspconfig').setup({
