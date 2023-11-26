32f5380c39fb10533f9febe7cc8fdbf7e7ecd6df
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 12:11:15 +0000

    Sync nvim config

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -5,6 +5,21 @@ return {
   },
   config = function()
     local lualine = require('lualine')
-    lualine.setup()
+    local lazy_status = require('lazy.status')
+
+    lualine.setup({
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates(),
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        }
+      }
+    })
   end
 }
