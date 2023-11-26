2f0bd949861e4f9b5a353931407fbcc2fe1fb1e8
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-26 12:55:43 +0000

    Sync nvim config

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -22,7 +22,7 @@ return {
           {
             function ()
               local status = require('lsp-status').status()
-              vim.notify(status)
+              print(status)
               return status
             end
           },
