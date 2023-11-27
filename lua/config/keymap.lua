5aa63b6132e9cea123d3d58652109eb349570abe
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-27 15:35:15 +0000

    Sync nvim config

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -4,6 +4,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
+-- redo
+vim.keymap.set("n", "U", '<C-r>')
+
 -- add tabs in visual mode
 vim.keymap.set('v', '<Tab>', '>gv', { noremap = true, silent = true })
 vim.keymap.set('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
