5420dfefe9b3dfef72e525c0448e9b07b5b84461
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-27 15:10:16 +0000

    Sync nvim config

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -9,26 +9,6 @@ return {
     'rafamadriz/friendly-snippets' -- set of predefined snippets
   },
   config = function()
-    -- -- Here is where you configure the autocompletion settings.
-    -- local lsp_zero = require('lsp-zero')
-    -- lsp_zero.extend_cmp()
-    --
-    -- -- And you can configure cmp even more, if you want to.
-    -- local cmp = require('cmp')
-    -- local cmp_action = lsp_zero.cmp_action()
-    --
-    -- cmp.setup({
-    --   formatting = lsp_zero.cmp_format(),
-    --   mapping = cmp.mapping.preset.insert({
-    --     ['<C-Space>'] = cmp.mapping.complete(),
-    --     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    --     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    --     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    --     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    --     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    --   })
-    -- })
-
     local cmp = require('cmp')
     local luasnip = require('luasnip')
 
@@ -57,6 +37,7 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
+        { name = 'nvim_lsp' }, -- nvims lsp suggestions
         { name = "luasnip" }, -- snippets
         { name = "buffer" },  -- text within current buffer
         { name = "path" },    -- file system paths
