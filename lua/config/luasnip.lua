b034f5e4be798187b4cc6c1f9297ac34b116e8ad
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-28 12:26:47 +0000

    Sync nvim config

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -1,88 +1,62 @@
--- luasnip.lua
+local ls = require('luasnip')
+local extras = require('luasnip.extras')
+local fmt = require('luasnip.extras.fmt').fmt
 
-local ls = require "luasnip"
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-local extras = require("luasnip.extras")
-local rep = extras.rep
-local fmt = require("luasnip.extras.fmt").fmt
-local c = ls.choice_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local sn = ls.snippet_node
-
-vim.keymap.set({ "i", "s" }, "<A-n>", function()
-    if ls.choice_active() then
-        ls.change_choice(1)
-    end
-end)
-
-vim.keymap.set({ "i", "s" }, "<A-k>", function()
-    if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
-    end
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<A-j>", function()
-    if ls.jumpable(-1) then
-        ls.jump(-1)
-    end
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
 end, { silent = true })
 
-ls.add_snippets("lua", {
-    s("hello", {
-        t('print("hello '),
-        i(1),
-        t(' world")')
-    }),
+vim.keymap.set({ "i", "s" }, "<C-n>", function()
+  if ls.choise_active() then
+    ls.change_choise(1)
+  end
+end, { silent = true })
 
-    s("if", {
-        t('if '),
-        i(1, "true"),
-        t(' then '),
-        i(2),
-        t(' end')
-    })
+ls.add_snippets('lua', {
+  ls.snippet('hello', {
+    ls.text_node('print("Hello '),
+    ls.insert_node(1),
+    ls.text_node(' world")')
+  }),
+  ls.snippet('if', fmt(
+    [[
+    if {} then
+      {}
+    end
+    ]], {
+      ls.insert_node(1),
+      ls.insert_node(2)
+    }
+  )),
+  -- ls.snippet('co', {
+  --   ls.dynamic_node(function()
+  --     local register_data = vim.fn.getreg() .. ""
+  --     if string.match(register_data, '[%d-]+,%s*[%d-]+') then
+  --       return ls.snippet_node(nil, {
+  --         ls.text_node('position([' .. register_data .. '])')
+  --       })
+  --     end
+  --   end)
+  -- })
 })
 
-ls.add_snippets("tex", {
-    s("beg", {
-        t("\\begin{"), i(1), t("}"),
-        t({ "", "\t" }), i(0),
-        t({ "", "\\end{" }), rep(1), t("}"),
-    })
+ls.add_snippets('typescriptreact', {
+  ls.snippet('co', {
+    ls.dynamic_node(function()
+      local register_data = vim.fn.getreg() .. ""
+      if string.match(register_data, '[%d-]+,%s*[%d-]+') then
+        return ls.snippet_node(nil, {
+          ls.text_node('position([' .. register_data .. '])')
+        })
+      end
+    end)
+  })
 })
-
-ls.add_snippets("cs", {
-    s("logc", 
-        fmt([[Debug.Log($"<color={}>{}</color>");]],
-        {
-            c(1, { 
-                t("red"), 
-                t("green"), 
-                t("blue"), 
-                t("cyan"), 
-                t("magenta") 
-            }),
-            i(2),
-        })),
-})
-
-ls.add_snippets("lua", {
-    s("co", {
-        d(function()
-            local register_data = vim.fn.getreg() .. "";
-            if string.match(register_data, "[%d-]+,%s*[%d-]+") then
-                return M.sn(nil, {
-                    M.t("position([" .. register_data .. "])"),
-                })
-            else
-                print("register does not contain the pattern")
-                return M.sn(nil, { })
-            end
-        end),
-        i(1)
-    })
-})
-
