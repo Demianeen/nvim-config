b62f6dbdcc54735693a9c3a3b83f5d836717e1f1
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-28 09:05:10 +0000

    Sync nvim config

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -1,5 +1,17 @@
 local ls = require('luasnip')
 
+vim.keymap.set({ "i", "s" }, "<A-k>", function()
+if ls.expand_or_jumpable() then
+ls. expand_or_jump ()
+end
+end, { silent = true })
+
+vim. keymap.set(‹ "i", "s" 3, "<A-j>", function()
+if ls. jumpable(-1) then
+ls. jump(-1)
+end
+end, { silent = true 3)
+
 ls.add_snippets('lua', {
   ls.snippet('hello', {
     ls.text_node('print("Hello '),
