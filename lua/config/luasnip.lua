bb723f3617ef19cd3c286575083f522ef4b6dd59
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-28 09:01:08 +0000

    Sync nvim config

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -2,6 +2,9 @@ local ls = require('luasnip')
 
 ls.add_snippets('lua', {
   ls.snippet('hello', {
-    ls.text_node('print("Hello world")')
+    ls.text_node('print("Hello'),
+    ls.insert_node(1),
+    ls.text_node('world)')
   })
 })
+
