c8bdb18615b6d2d2a4fcd17a0c953f0212474361
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-29 10:46:30 +0000

    Sync nvim config

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -1,66 +1,66 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-        local conform = require('conform')
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require('conform')
 
-        -- sub-list runs only the first available formatter
-        local prettierFormatter = {
-            'prettierd',
-            'prettier',
-        }
+    -- sub-list runs only the first available formatter
+    local prettierFormatter = {
+      'prettierd',
+      'prettier',
+    }
 
-        local format_options = {
-            -- falback to lsp if formatters are not avaliable
-            lsp_fallback = true,
-            timeout_ms = 500,
-        }
+    local format_options = {
+      -- falback to lsp if formatters are not avaliable
+      lsp_fallback = true,
+      timeout_ms = 500,
+    }
 
-        conform.setup({
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                python = { 'isort', 'black' },
-                javascript = {
-                    prettierFormatter,
-                },
-                javascriptreact = {
-                    prettierFormatter,
-                },
+    conform.setup({
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        javascript = {
+          prettierFormatter,
+        },
+        javascriptreact = {
+          prettierFormatter,
+        },
 
-                typescript = {
-                    prettierFormatter,
-                },
-                typescriptreact = {
-                    prettierFormatter,
-                },
+        typescript = {
+          prettierFormatter,
+        },
+        typescriptreact = {
+          prettierFormatter,
+        },
 
-                svelte = {
-                    prettierFormatter,
-                },
-                css = {
-                    prettierFormatter,
-                },
-                html = {
-                    prettierFormatter,
-                },
-                josn = {
-                    prettierFormatter,
-                },
-                yaml = {
-                    prettierFormatter,
-                },
-                markdown = {
-                    prettierFormatter,
-                },
-            },
-            format_on_save = format_options,
-            notify_on_error = true,
-        })
+        svelte = {
+          prettierFormatter,
+        },
+        css = {
+          prettierFormatter,
+        },
+        html = {
+          prettierFormatter,
+        },
+        josn = {
+          prettierFormatter,
+        },
+        yaml = {
+          prettierFormatter,
+        },
+        markdown = {
+          prettierFormatter,
+        },
+      },
+      format_on_save = format_options,
+      notify_on_error = true,
+    })
 
-        vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
-            conform.format(format_options)
-        end, {
-            desc = 'Format file or range(in visual mode)',
-        })
-    end,
+    vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
+      conform.format(format_options)
+    end, {
+      desc = 'Format file or range(in visual mode)',
+    })
+  end,
 }
