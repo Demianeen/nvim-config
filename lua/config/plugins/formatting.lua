ba55ef98ee2ac42025aeadfb1709232c6f3a8df7
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-29 10:38:57 +0000

    Sync nvim config

---
+++ lua/config/plugins/formatting.lua
@@ -0,0 +1,63 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		-- sub-list runs only the first available formatter
+		local prettierFormatter = prettierFormatter
+
+		local format_options = {
+			-- falback to lsp if formatters are not avaliable
+			lsp_fallback = true,
+			timeout_ms = 500,
+		}
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = {
+					prettierFormatter,
+				},
+				javascriptreact = {
+					prettierFormatter,
+				},
+
+				typescript = {
+					prettierFormatter,
+				},
+				typescriptreact = {
+					prettierFormatter,
+				},
+
+				svelte = {
+					prettierFormatter,
+				},
+				css = {
+					prettierFormatter,
+				},
+				html = {
+					prettierFormatter,
+				},
+				josn = {
+					prettierFormatter,
+				},
+				yaml = {
+					prettierFormatter,
+				},
+				markdown = {
+					prettierFormatter,
+				},
+			},
+			format_on_save = format_options,
+			notify_on_error = true,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format(format_options)
+		end, {
+			desc = "Format file or range(in visual mode)",
+		})
+	end,
+}
