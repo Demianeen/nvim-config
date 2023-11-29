28774e8c05db8e4473da7d1fbea7abda5936ca86
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-29 11:54:29 +0000

    Sync nvim config

--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -5,26 +5,43 @@ return {
     config = function()
       local mason = require('mason')
       local mason_lspconfig = require('mason-lspconfig')
+      local mason_tool_installer = require('mason-tool-installer')
 
       mason.setup()
 
       mason_lspconfig.setup({
         ensure_installed = {
-          'cssls',  -- css-lsp
+          -- lsp
+          'cssls', -- css-lsp
           'eslint', -- eslint-lsp
-          'html',   -- html-lsp
+          'html', -- html-lsp
           'lua_ls', -- lua-language-server
           'stylelint_lsp', -- stylelint-lsp
-          'tsserver',      -- typescript-language-server
-          'vimls',         -- vim-language-server
+          'tsserver', -- typescript-language-server
+          'vimls', -- vim-language-server
           'vtsls',
-          'emmet_ls'
+          'emmet_ls',
         },
-        automatic_installation = true -- automaticly installs lsps when you open file without them
+        automatic_installation = true, -- automaticly installs lsps when you open file without them
+      })
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          -- linters
+          'pylint',
+          'selene',
+          -- formatters
+          'stylua',
+          'black',
+          'isort',
+          'prettierd',
+          'prettier',
+        },
       })
     end,
     dependencies = {
-      'williamboman/mason-lspconfig.nvim'
-    }
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+    },
   },
 }
