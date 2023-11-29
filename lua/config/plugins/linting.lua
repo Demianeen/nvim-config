f8a8a6fdabef41ef8cca2e0757975b1964e9ad04
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-29 11:32:04 +0000

    Sync nvim config

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -9,5 +9,14 @@ return {
       lua = { 'selene' },
       luau = { 'selene ' },
     }
+
+    local lint_autogroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_autogroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
   end,
 }
