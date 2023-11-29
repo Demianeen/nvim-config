933aca30dc9a4dafc6c0c3f1e0fabdb126091958
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-29 11:05:28 +0000

    Sync nvim config

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,8 +1,8 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local nvimtree = require("nvim-tree")
+    local nvimtree = require('nvim-tree')
 
     -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
@@ -13,18 +13,34 @@ return {
     vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
 
     local function on_attach(bufnr)
-      local api = require "nvim-tree.api"
+      local api = require('nvim-tree.api')
 
       local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        return {
+          desc = 'nvim-tree: ' .. desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true,
+        }
       end
 
       -- default mappings
       api.config.mappings.default_on_attach(bufnr)
 
       -- custom mappings
-      vim.keymap.set('n', 'p', api.tree.change_root_to_parent, opts('Up to parent'))
-      vim.keymap.set('n', 'P', api.tree.change_root_to_parent, opts('Open parent folder in explorer'))
+      vim.keymap.set(
+        'n',
+        'p',
+        api.tree.change_root_to_parent,
+        opts('Up to parent')
+      )
+      vim.keymap.set(
+        'n',
+        'P',
+        api.tree.change_root_to_parent,
+        opts('Open parent folder in explorer')
+      )
       vim.keymap.set('n', 'l', 'o', opts('Open file'))
       vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
     end
@@ -44,8 +60,8 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+              arrow_closed = '', -- arrow when folder is closed
+              arrow_open = '', -- arrow when folder is open
             },
           },
         },
@@ -58,10 +74,11 @@ return {
           window_picker = {
             enable = false,
           },
+          quit_on_open = true,
         },
       },
       filters = {
-        custom = { ".DS_Store" },
+        custom = { '.DS_Store' },
       },
       git = {
         ignore = true,
@@ -73,12 +90,26 @@ return {
       hijack_unnamed_buffer_when_opening = true,
       -- still haven't figure out what it is. But nvim-tree do recommend it to set to true when using dressing.nvim
       select_prompts = true,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
 
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })   -- toggle file explorer
-    vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>",
-      { desc = "Toggle file explorer on current file" })                                              -- toggle file explorer on current file
-    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    vim.keymap.set(
+      'n',
+      '<leader>ee',
+      '<cmd>NvimTreeToggle<CR>',
+      { desc = 'Toggle file explorer' }
+    ) -- toggle file explorer
+    vim.keymap.set(
+      'n',
+      '<leader>ef',
+      '<cmd>NvimTreeFindFileToggle<CR>',
+      { desc = 'Toggle file explorer on current file' }
+    ) -- toggle file explorer on current file
+    vim.keymap.set(
+      'n',
+      '<leader>er',
+      '<cmd>NvimTreeRefresh<CR>',
+      { desc = 'Refresh file explorer' }
+    ) -- refresh file explorer
   end,
 }
