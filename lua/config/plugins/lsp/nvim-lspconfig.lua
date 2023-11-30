b9558dc5b91649720205979a367a27ee4455325c
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-30 08:39:16 +0000

    Sync nvim config

--- lua/config/plugins/lsp/nvim-lspconfig.lua
+++ lua/config/plugins/lsp/nvim-lspconfig.lua
@@ -8,111 +8,132 @@ return {
       'antosha417/nvim-lsp-file-operations',
       config = true,
       dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-neo-tree/neo-tree.nvim",
-      }
-    }
+        'nvim-lua/plenary.nvim',
+        'nvim-neo-tree/neo-tree.nvim',
+      },
+    },
   },
   config = function()
     local lspconfig = require('lspconfig')
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
     local opts = { noremap = true, silent = true }
     local on_attach = function(client, bufnr)
       opts.buffer = bufnr
 
       -- keymaps
-      opts.desc = "Show LSP references"
-      vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      opts.desc = 'Show LSP references'
+      vim.keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts) -- show definition, references
 
-      opts.desc = "Go to declaration"
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      opts.desc = 'Go to declaration'
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- go to declaration
 
-      opts.desc = "Show LSP definitions"
-      vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      opts.desc = 'Show LSP definitions'
+      vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts) -- show lsp definitions
 
-      opts.desc = "Show LSP implementations"
-      vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      opts.desc = 'Show LSP implementations'
+      vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
 
-      opts.desc = "Show LSP type definitions"
-      vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      opts.desc = 'Show LSP type definitions'
+      vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
 
-      opts.desc = "See available code actions"
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      opts.desc = 'See available code actions'
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-      opts.desc = "Smart rename"
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      opts.desc = 'Smart rename'
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
 
-      opts.desc = "Show buffer diagnostics"
-      vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      opts.desc = 'Show buffer diagnostics'
+      vim.keymap.set(
+        'n',
+        '<leader>D',
+        '<cmd>Telescope diagnostics bufnr=0<CR>',
+        opts
+      ) -- show  diagnostics for file
 
-      opts.desc = "Show line diagnostics"
-      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+      opts.desc = 'Show line diagnostics'
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-      opts.desc = "Go to previous diagnostic"
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      opts.desc = 'Go to previous diagnostic'
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-      opts.desc = "Go to next diagnostic"
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      opts.desc = 'Go to next diagnostic'
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-      opts.desc = "Show documentation for what is under cursor"
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      opts.desc = 'Show documentation for what is under cursor'
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-      opts.desc = "Restart LSP"
-      vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      opts.desc = 'Restart LSP'
+      vim.keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts) -- mapping to restart lsp if necessary
     end
 
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- languages configurations
-    lspconfig["lua_ls"].setup({
+    lspconfig['lua_ls'].setup({
       capabilities = capabilities,
       on_attach = on_attach,
       on_init = function(client)
         local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT'
+        if
+          not vim.loop.fs_stat(path .. '/.luarc.json')
+          and not vim.loop.fs_stat(path .. '/.luarc.jsonc')
+        then
+          client.config.settings =
+            vim.tbl_deep_extend('force', client.config.settings, {
+              Lua = {
+                runtime = {
+                  -- Tell the language server which version of Lua you're using
+                  -- (most likely LuaJIT in the case of Neovim)
+                  version = 'LuaJIT',
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME,
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                  },
+                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                  -- library = vim.api.nvim_get_runtime_file("", true)
+                },
               },
-              -- Make the server aware of Neovim runtime files
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  vim.env.VIMRUNTIME
-                  -- "${3rd}/luv/library"
-                  -- "${3rd}/busted/library",
-                }
-                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                -- library = vim.api.nvim_get_runtime_file("", true)
-              }
-            }
-          })
+            })
 
-          client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          client.notify(
+            'workspace/didChangeConfiguration',
+            { settings = client.config.settings }
+          )
         end
         return true
-      end
+      end,
     })
 
     lspconfig['html'].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
 
     lspconfig['emmet_ls'].setup({
       capabilities = capabilities,
       on_attach = on_attach,
-      filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'scss', 'sass', 'less', 'svelte' }
+      filetypes = {
+        'html',
+        'typescriptreact',
+        'javascriptreact',
+        'css',
+        'scss',
+        'sass',
+        'less',
+        'svelte',
+      },
     })
 
     lspconfig['cssls'].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
 
     lspconfig['stylelint_lsp'].setup({
@@ -120,25 +141,25 @@ return {
       on_attach = on_attach,
       settings = {
         autoFixOnFormat = true,
-        autoFixOnSave = true
-      }
+        autoFixOnSave = true,
+      },
     })
 
     lspconfig['vtsls'].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
 
     lspconfig['tsserver'].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
 
     lspconfig['eslint'].setup({
       on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
+        vim.api.nvim_create_autocmd('BufWritePre', {
           buffer = bufnr,
-          command = "EslintFixAll",
+          command = 'EslintFixAll',
         })
         on_attach(client, bufnr)
       end,
@@ -146,7 +167,12 @@ return {
 
     lspconfig['terraform_lsp'].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
-  end
+
+    lspconfig['grammarly'].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
 }
