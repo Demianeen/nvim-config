610cbab921dfaa4a291d0061a07092fbdf15ad70
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-30 06:21:31 +0000

    Sync nvim config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,30 +1,30 @@
 -- installs lazy if not installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- loads lazy and plugins
-require("lazy").setup({
-  { import = "config.plugins" },
-  { import = "config.plugins.lsp" }
+require('lazy').setup({
+  { import = 'config.plugins' },
+  { import = 'config.plugins.lsp' },
 }, {
   install = {
-    colorscheme = { 'tokyonight' }
+    colorscheme = { 'catppuccin' },
   },
   checker = {
     enabled = true,
-    notify = false
+    notify = false,
   },
   change_detection = {
-    notify = false
-  }
+    notify = false,
+  },
 })
