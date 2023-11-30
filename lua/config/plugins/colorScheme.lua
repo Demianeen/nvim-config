3aea107c4fbe7b10e6236f2c881e1dcac104d39c
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-11-30 06:34:31 +0000

    Sync nvim config

--- lua/config/plugins/colorScheme.lua
+++ lua/config/plugins/colorScheme.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000,
   config = function()
     require('catppuccin').setup({
-      flavour = 'macchiato',
+      flavour = 'mocha',
       integrations = {
         cmp = true,
         -- gitsigns = true,
