30f21f29771a46ea09f0520fa7de17f2a34998ed
Feliche-Demian Netliukh <dnetlyukh@gmail.com>
2023-12-01 09:26:17 +0000

    Sync nvim config

--- lua/config/plugins/tree-sitter.lua
+++ lua/config/plugins/tree-sitter.lua
@@ -1,14 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    local configs = require('nvim-treesitter.configs')
 
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "css", "html" },
+      ensure_installed = {
+        'lua',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'typescript',
+        'css',
+        'html',
+      },
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
-  end
+  end,
 }
